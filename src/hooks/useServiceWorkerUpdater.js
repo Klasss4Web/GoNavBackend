f3cefd77f60babe82a42022b7312b3684ef2f6d9
emmanuel.ochade <emmanuel.ochade@interswitchng.com>
2025-08-23 17:55:32 +0100

    made the desktop devices responsive

--- src/hooks/useServiceWorkerUpdater.js
+++ src/hooks/useServiceWorkerUpdater.js
@@ -26,13 +26,7 @@ export function useServiceWorkerUpdater() {
   const updateServiceWorker = () => {
     if (waitingWorker) {
       waitingWorker.postMessage({ type: "SKIP_WAITING" });
-      waitingWorker.addEventListener("statechange", (e) => {
-        console.log({ e });
-        if (e.target.state === "activated") {
-          window.location.reload();
-          setWaitingWorker(null);
-        }
-      });
+      setWaitingWorker(null); // ✅ Close toast after updating
     }
   };
 
